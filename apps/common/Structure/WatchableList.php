376e349ddc28afb9e750975b1e0e9dc081a6e30d
Setya Wibawa <durianpeople@gmail.com>
2020-05-03 19:07:57 +0700

    watchable list update

--- apps/common/Structure/WatchableList.php
+++ apps/common/Structure/WatchableList.php
@@ -4,12 +4,21 @@ namespace Common\Structure;
 
 use Common\Interfaces\EqualityComparable;
 
+/**
+ * Watchable list. This list can track added and removed items.
+ * 
+ * Items contained in this list must implement EqualityComparable.
+ * 
+ * This list cannot contain duplicate items.
+ */
 class WatchableList
 {
     /** @var EqualityComparable[] */
     protected array $added_items = [];
-    /** @var EqualityComparable */
+    /** @var EqualityComparable[] */
     protected array $current_items = [];
+    /** @var EqualityComparable[] */
+    protected array $initial_items = [];
     /** @var EqualityComparable[] */
     protected array $removed_items = [];
 
@@ -23,53 +32,74 @@ class WatchableList
         $this->watched_class = $watched_class;
     }
 
+    /**
+     * Optional. Initialize list with items
+     *
+     * @param array $items
+     * @return void
+     */
+    public function initializeItems(array $items)
+    {
+        if (!empty($this->initial_items)) throw new \RuntimeException("List is already initialized");
+
+        foreach ($items as $item) {
+            assert($item instanceof $this->watched_class, new \TypeError("Item must be a type of " . $this->watched_class));
+        }
+
+        $this->current_items = $items;
+        $this->initial_items = $items;
+    }
+
+    /**
+     * Add item to watchable list
+     *
+     * @param mixed $item
+     * @return void
+     */
     public function add($item)
     {
-        assert(($item instanceof $this->watched_class), new \TypeError("Item must be a type of " . $this->watched_class));
+        $this->assertItemType($item);
 
-        $i = 0;
-        foreach ($this->removed_items as $r) {
-            if ($r->equals($item)) {
-                unset($this->removed_items[$i]);
-            }
-            $i++;
+        if ($this->isRemovedItem($item)) {
+            $this->removeFromRemoved($item);
         }
 
-        $i = 0;
-        foreach ($this->current_items as $r) {
-            if ($r->equals($item)) {
-                unset($this->current_items[$i]);
-            }
-            $i++;
+        if (!$this->isAddedItem($item) && !$this->isInitialItem($item)) {
+            $this->added_items[] = $item;
         }
 
-        $this->added_items[] = $item;
-        $this->current_items[] = $item;
+        if (!$this->isCurrentItem($item)) {
+            $this->current_items[] = $item;
+        }
     }
 
+    /**
+     * Remove item from watchable list
+     *
+     * @param mixed $item
+     * @return void
+     */
     public function remove($item)
     {
-        assert(($item instanceof $this->watched_class), new \TypeError("Item must be a type of " . $this->watched_class));
+        $this->assertItemType($item);
 
-        $i = 0;
-        foreach ($this->added_items as $a) {
-            if ($a->equals($item)) {
-                unset($this->added_items[$i]);
-            }
-            $i++;
+        $this->removeFromCurrent($item);
+
+        if ($this->isAddedItem($item)) {
+            $this->removeFromAdded($item);
+            return;
         }
 
-        $i = 0;
-        foreach ($this->current_items as $a) {
-            if ($a->equals($item)) {
-                unset($this->current_items[$i]);
-            }
-            $i++;
+        if (!$this->isRemovedItem($item)) {
+            $this->removed_items[] = $item;
         }
-
-        $this->removed_items[] = $item;
     }
 
+    /**
+     * Count items in this list
+     *
+     * @return integer
+     */
     public function count(): int
     {
         return count($this->current_items);
@@ -90,12 +120,72 @@ class WatchableList
         return $this->current_items;
     }
 
-    public function setCurrentItems(array $items): array
+    private function assertItemType($item)
     {
-        foreach ($items as $item) {
-            assert($item instanceof $this->watched_class, new \TypeError("Item must be a type of " . $this->watched_class));
-        }
+        assert(($item instanceof $this->watched_class), new \TypeError("Item must be a type of " . $this->watched_class));
+    }
 
-        return $this->current_items = $items;
+    private function isInitialItem($item): bool
+    {
+        foreach ($this->initial_items as $i) {
+            if ($i->equals($item)) return true;
+        }
+        return false;
+    }
+
+    private function isAddedItem($item): bool
+    {
+        foreach ($this->added_items as $i) {
+            if ($i->equals($item)) return true;
+        }
+        return false;
+    }
+
+    private function isRemovedItem($item): bool
+    {
+        foreach ($this->removed_items as $i) {
+            if ($i->equals($item)) return true;
+        }
+        return false;
+    }
+
+    private function isCurrentItem($item): bool
+    {
+        foreach ($this->current_items as $i) {
+            if ($i->equals($item)) return true;
+        }
+        return false;
+    }
+
+    private function removeFromAdded($item)
+    {
+        foreach ($this->added_items as $key => $i) {
+            if ($i->equals($item)) {
+                unset($this->added_items[$key]);
+                break;
+            }
+        }
+    }
+
+    private function removeFromRemoved($item)
+    {
+        foreach ($this->removed_items as $key => $i) {
+            if ($i->equals($item)) {
+                unset($this->removed_items[$key]);
+                break;
+            }
+        }
+        return false;
+    }
+
+    private function removeFromCurrent($item)
+    {
+        foreach ($this->current_items as $key => $i) {
+            if ($i->equals($item)) {
+                unset($this->current_items[$key]);
+                break;
+            }
+        }
+        return false;
     }
 }
