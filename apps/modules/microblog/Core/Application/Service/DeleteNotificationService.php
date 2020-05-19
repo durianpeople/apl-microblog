f9fc5afcf7846727f23ce39c9367ad32d90d12fd
Setya Wibawa <durianpeople@gmail.com>
2020-05-19 21:31:14 +0700

    notification done

---
+++ apps/modules/microblog/Core/Application/Service/DeleteNotificationService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Microblog\Core\Application\Service;
+
+use Microblog\Core\Application\Request\DeleteNotificationRequest;
+use Microblog\Core\Domain\Interfaces\IUserRepository;
+use Microblog\Core\Domain\Model\User\NotificationID;
+use Microblog\Core\Domain\Model\User\UserID;
+
+class DeleteNotificationService
+{
+    protected IUserRepository $user_repo;
+
+    public function __construct(IUserRepository $user_repo)
+    {
+        $this->user_repo = $user_repo;
+    }
+
+    public function execute(DeleteNotificationRequest $request)
+    {
+        $user = $this->user_repo->find(new UserID($request->owner_id));
+        $nid = new NotificationID($user->id, $request->guid);
+        $this->user_repo->populateNotifications($user);
+
+        foreach ($user->current_notifications as $n) {
+            if ($n->id->equals($nid)) {
+                $user->deleteNotification($n);
+                break;
+            }
+        }
+
+        $this->user_repo->persist($user);
+    }
+}
