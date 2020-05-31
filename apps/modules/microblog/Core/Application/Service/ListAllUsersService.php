6d20a2e48e626e0d1fe3ceeca4f4f49858acdffe
yovi <yoviagustian94@gmail.com>
2020-06-01 01:34:34 +0700

    search controller and service

--- apps/modules/microblog/Core/Application/Service/ListAllUsersService.php
+++ apps/modules/microblog/Core/Application/Service/ListAllUsersService.php
@@ -2,7 +2,8 @@
 
 namespace Microblog\Core\Application\Service;
 
-use Microblog\Core\Application\Request\ListAllUsers;
+use Microblog\Core\Application\Request\ListAllUsersRequest;
+use Microblog\Core\Application\Response\UserInfo;
 use Microblog\Core\Domain\Repository\IUserRepository;
 use Microblog\Core\Domain\Model\User\UserID;
 
@@ -12,43 +13,24 @@ class ListAllUsersService
 
     public function __construct(IUserRepository $user_repo)
     {
-        $this->post_repo = $post_repo;
         $this->user_repo = $user_repo;
     }
 
     /**
      * 
      *
-     * @param ListAllUsersIDRequest $request
+     * @param ListAllUsersRequest $request
      * @return UserInfo[]
      */
-    public function execute(ListAllPostsByUserIDRequest $request): array
+    public function execute(ListAllUsersRequest $request): array
     {
-        $post_infos = [];
+        $user_infos = [];
 
-        $user = $this->user_repo->find(new UserID($request->user_id));
-
-        foreach ($user->current_followings as $followee_id) {
-            $posts = $this->post_repo->getPostsByUserID($followee_id);
-
-            foreach ($posts as $p) {
-                $user = $this->user_repo->find($p->poster_id);
-                $post_infos[] = PostInfo::create($p, $user->username->getString());
-            }
+        $users = $this->user_repo->getAll();
+        foreach ($users as $u) {
+            $user_infos[] = new UserInfo($u);
         }
 
-        $posts = $this->post_repo->getPostsByUserID(new UserID($request->user_id));
-
-        foreach ($posts as $p) {
-            $user = $this->user_repo->find($p->poster_id);
-            $post_infos[] = PostInfo::create($p, $user->username->getString());
-        }
-
-        usort($post_infos, function($a, $b) {
-            return -1 * strcmp($a->created_at, $b->created_at);
-        });
-
-
-        return $post_infos;
+        return $user_infos;
     }
 }
